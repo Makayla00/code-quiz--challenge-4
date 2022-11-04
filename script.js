2cb5c7939aa7817d809d17de84515ce08fbc8f2f
Makayla00 <115745187+Makayla00@users.noreply.github.com>
2022-11-04 15:06:15 -0600

    set up HTML file started psudocode and added to js file

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var questions = [
+// question 1
+{   title : "this is a question",
+    choices : " a, b , c , d ",
+    answer : "the answer"
+},
+// question 2
+{   title : "this is a question",
+    choices : " a, b , c , d ",
+    answer : "the answer"
+},
+// question 3
+{   title : "this is a question",
+    choices : " a, b , c , d ",
+    answer : "the answer"
+},
+// question 4
+{   title : "this is a question",
+    choices : " a, b , c , d ",
+    answer : "the answer"
+},
+// question 5
+{   title : "this is a question",
+    choices : " a, b , c , d ",
+    answer : "the answer"
+},
+]
